b5161979f831d00127f43eab9f431063cb77b7a5
Ubuntu <ubuntu@ip-172-31-71-124.ec2.internal>
2019-08-19 23:30:45 +0000

    starting to create jma training routiine

--- prep/h5_to_hkl_jma.py
+++ prep/h5_to_hkl_jma.py
@@ -1,14 +1,45 @@
 #
 # convert jma rada data in h5 format into hkl data
 # for prednet
-# 
+#
+import os
+import glob
+
 import numpy as np
 import hickle as hkl
+import h5py
 
-hkl.dump(in_video, dname+fname+'_data.hkl', mode='w')
-    
-save_as_hickle(0,6000,dname,'mnist_tain_6000')
-save_as_hickle(6000,8000,dname,'mnist_valid_2000')
-save_as_hickle(8000,10000,dname,'mnist_test_2000')
+def save_as_hickle(dname,pattern,case):
+    print('save_as_hickle',dname,pattern,case)
+    #pattern = '../data/jma/data_kanto_tmp/*h5'
+    #flist = os.listdir(dirname)
+    flist = glob.glob(pattern)
+    flist.sort()
 
+    #print(list)
+    nt = 12 # time steps per data (1h)
+    X_sources = np.zeros(len(flist)*nt)
+    X_all = np.zeros((len(flist)*nt,200,200,1))
 
+    for i,fname in enumerate(flist):
+        print('reading:',i,fname)
+        h5file = h5py.File(fname,'r')
+        X = h5file['R'][()]
+        X = np.maximum(X,0) # replace negative value with 0
+        X = X/201.0*255.0 # scale range to [0-255]
+        X = X[:,:,:,None] # add "channel" dimension as 1 (channel-last format)
+        h5file.close()
+        i1 = i*nt
+        i2 = (i+1)*nt
+        X_all[i1:i2,:,:,:] = X
+        X_sources[i1:i2] = i
+
+    hkl.dump(X_all, dname+case+'_data.hkl', mode='w')
+    hkl.dump(X_sources, dname+case+'_sources.hkl', mode='w')
+
+dname = '../data/jma/'
+# 2015 and 2016 data for training 
+#save_as_hickle(dname,'../data/jma/data_kanto/2p-jmaradar5_201[56]*h5','jma_train_2015-2016')
+save_as_hickle(dname,'../data/jma/data_kanto/2p-jmaradar5_2015*h5','jma_train_2015')
+# 2017 data to test
+save_as_hickle(dname,'../data/jma/data_kanto/2p-jmaradar5_2017*h5','jma_test_2017')
---
+++ src/settings_jma.py
@@ -0,0 +1,8 @@
+# Where data will be stored
+DATA_DIR = '../data/data/'
+
+# Where model weights and config will be saved if you run *_train.py
+WEIGHTS_DIR = '../result/'
+
+# Where results (prediction plots and evaluation file) will be saved.
+RESULTS_SAVE_DIR = '../result/'
